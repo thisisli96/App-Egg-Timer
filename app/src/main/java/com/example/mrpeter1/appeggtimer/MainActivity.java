38c4c2a2addf7b6d17d912f0718f9b976dd16b68
muh ikhsan <thisisli96@yahoo.com>
2019-12-09 22:02:13 +0700

    timer back  dengan button

--- app/src/main/java/com/example/mrpeter1/appeggtimer/MainActivity.java
+++ app/src/main/java/com/example/mrpeter1/appeggtimer/MainActivity.java
@@ -3,37 +3,65 @@ package com.example.mrpeter1.appeggtimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button buttonplayback;
+    TextView textView ;
+    SeekBar VolumeSeekBar;
+    public void timerback (View view){
+        CountDownTimer countDownTimer = new CountDownTimer(VolumeSeekBar.getProgress()*1000 + 100, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                updateTimer((int) millisUntilFinished / 1000);
+            }
 
+            @Override
+            public void onFinish() {
+                Log.i("tag", "timer all done");
+            }
+        }.start();
+
+    };
+
+
+    public void updateTimer(int secondleft){
+
+        int minute = secondleft / 60;
+        int seconds = secondleft - (minute* 60);
+
+        String secondString = Integer.toString(seconds);
+        if (seconds <= 9 ){
+            secondString = "0" + secondString;
+        }
+        textView.setText(Integer.toString(minute)+ ":"+ secondString);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-      final TextView textView = findViewById(R.id.textView);
+
+       textView = findViewById(R.id.textView);
+      buttonplayback = findViewById(R.id.buttonplayback);
         //textView2 = findViewById(R.id.textView2);
 
-        final SeekBar VolumeSeekBar = (SeekBar) findViewById(R.id.VolumeSeekBar);
+     VolumeSeekBar = findViewById(R.id.VolumeSeekBar);
 
         VolumeSeekBar.setMax(1000); // nilai maksimal
-        VolumeSeekBar.setProgress(200);
+        //VolumeSeekBar.setProgress(0);
 
         VolumeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                int minute = progress / 60;
-                int seconds = progress - (minute* 60);
-
-                String secondString = Integer.toString(seconds);
-                if (secondString == "0"){
-                    secondString = "00";
-                }
-                textView.setText(Integer.toString(minute)+ ":"+ secondString);
+            public void onProgressChanged(SeekBar seekBar, final int progress, boolean fromUser) {
+                updateTimer(progress);
 
             }
 
